c87e32342ba1f67f60b7ddae9ee5d88b74c0a5e5
Anastasija <78434027+Anastasia1207@users.noreply.github.com>
2021-04-17 12:35:36 +0300

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,9 @@
+// var sbt_btn = document.querySelector('#submit_button');
+// function coloration_button(){
+//    if(sbt_btn.disabled  == true){
+//      sbt_btn.style.background = 'red';
+//    }
+//   else { 
+//      sbt_btn.style.background = 'green';
+//    }
+// }
